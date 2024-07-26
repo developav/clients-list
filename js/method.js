3cf0f4793e42238477b4c748d288163290bbc495
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2024-07-26 14:28:19 +0300

    run the localserver

--- js/method.js
+++ js/method.js
@@ -2,11 +2,10 @@
 import { filterClientsaByName } from './tranformData.js';
 import { generateTable } from './generateTable.js';
 // Функция получения списка клиентов
-const URL_CLIENT_LIST = new URL('https://dpavlov.flerken.space/api/clients')
+const URL_CLIENT_LIST = new URL('http://localhost:3000/api/clients')
 export async function clientsGet() {
   try{
     const response = await fetch(URL_CLIENT_LIST, {
-      mode: 'no-cors',
         method: 'GET',
         headers: {'Content-type': 'application/json'},
       });
